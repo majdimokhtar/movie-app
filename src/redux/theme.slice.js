21d6c77f7fe411f697e76cd5a6ce4ccd40e39d2f
unknown <majdi.mokhtar@gmail.com>
2022-09-12 23:04:31 +0100

    finishing touches

--- src/redux/theme.slice.js
+++ src/redux/theme.slice.js
@@ -1,24 +1,3 @@
-// import { createSlice } from "@reduxjs/toolkit";
-
-// const initialState = {
-//   mode: "browser", // browser || light || dark
-// };
-
-// const themeSlice = createSlice({
-//   name: "theme",
-//   initialState,
-//   reducers: {
-//     setThemeMode: (state, action) => {
-//       state.mode = action.payload;
-//     },
-//   },
-// });
-
-// export const { setDarkMode, setThemeMode } = themeSlice.actions;
-
-// export default themeSlice.reducer;
-
-
 import { createSlice } from "@reduxjs/toolkit";
 
 export const themeSlice = createSlice({
